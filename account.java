37d313274b96c2c16123e488171786d8bdc1b3fa
salomon <salomon.cloud.dev@gmail.com>
2024-07-01 16:35:28 -0400

    java

---
+++ account.java
@@ -0,0 +1,18 @@
+package structure;
+
+public class account {
+    String owner;
+    double balance;
+    double minimumBalance;
+
+    public void deposit(double amount) {
+        // implementation of method
+    }
+
+    public void withdraw(double amount) {
+        // implementation
+    }
+}
+
+// this is an example of creating classes - using a banking system as example
+// now i'll create instances that will use this class
