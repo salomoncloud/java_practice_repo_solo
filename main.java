9986732bc8121f41e2ff196b85cfe76b783ce771
salomon <salomon.cloud.dev@gmail.com>
2024-07-01 15:51:59 -0400

    java practice

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
+
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-        System.out.println("hey");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hey, whats your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
     }
 }
